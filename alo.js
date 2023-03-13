978b0f519d953571d53cbe46bc3e13afbf0b7632
rakibal123 <rakibalmahmud22@gmail.com>
2023-03-14 04:44:03 +0600

    first commit

---
+++ alo.js
@@ -0,0 +1,10 @@
+function sumAray(number){
+    let sum =0;
+    for(var i=0; i<number.length; i++){
+        sum = sum + i;
+        
+    }
+    return sum;
+}
+ var myNumber =number([12,12,12])
+ console.log(myNumber);
